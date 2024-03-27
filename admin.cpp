a48e2505408128d08566ee4e308047412e517ea6
Max <maxooonzzz@gmail.com>
2024-03-27 21:59:29 +0200

    87.5%

--- admin.cpp
+++ admin.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "admin.h"
+#include "validationFile.h"
 #include "moviesDB.h"
 #include "seriesDB.h"
 #include <iostream>
@@ -15,20 +16,18 @@ void Admin::addMovie() {
 	Movie currentMovie;
 	time_t currTime = time(nullptr);
 	string path = "movies.txt";
-	string name;
-	string category;
+	string name, message, category;
 	int year, length;
 	ofstream fout;
 	cout << "Which movie you want to add?" << endl;
-	cout << "Name?" << endl;
-	cin.get();
-	getline(cin, name);
+	message = "Name?\n";
+	name = answerStringViewer(message, 0, 2, 25);
 	cout << "Category?" << endl;
 	category = chooseCategory();
-	cout << "Year of release?" << endl;
-	cin >> year;
-	cout << "Length?" << endl;
-	cin >> length;
+	message = "Year of release?\n";
+	year = answerIntViewer(message, 1950, 2024);
+	message = "Length?\n";
+	length = answerIntViewer(message, 30, 300);
 	currentMovie.setName(name);
 	currentMovie.setCategory(category);
 	currentMovie.setYear(year);
@@ -41,23 +40,21 @@ void Admin::addSeries() {
 	time_t currTime = time(nullptr);
 	Series currentSeries;
 	string path = "series.txt";
-	string name;
-	string category;
+	string name, category, message;
 	int year, seasons, episodes;
 	ofstream fout;
 
 	cout << "Which series you want to add?" << endl;
-	cout << "Name?" << endl;
-	cin.get();
-	getline(cin, name);
+	message = "Name?\n";
+	name = answerStringViewer(message, 0, 2, 25);
 	cout << "Category?" << endl;
 	category = chooseCategory();
-	cout << "Year of release?" << endl;
-	cin >> year;
-	cout << "How much seasons in series?" << endl;
-	cin >> seasons;
-	cout << "How much episodes in every season?" << endl;
-	cin >> episodes;
+	message = "Year of release?\n";
+	year = answerIntViewer(message, 1950, 2024);
+	message = "How much seasons in series?\n";
+	seasons = answerIntViewer(message, 1, 30);
+	message = "How much episodes in every season?\n";
+	episodes = answerIntViewer(message, 1, 30);
 	currentSeries.setName(name);
 	currentSeries.setCategory(category);
 	currentSeries.setYear(year);
@@ -69,7 +66,7 @@ void Admin::addSeries() {
 
 void Admin::findMovieByName() {
 	int answer = 0;
-	string name;
+	string name, message;
 	vector<Movie>& movies = MoviesDB::getMoviesDB();
 	vector<Movie>& watchListMovies = MoviesDB::getMoviesWatchListDB();
 
@@ -78,41 +75,39 @@ void Admin::findMovieByName() {
 		return;
 	}
 
-	cout << "Write name of movie you want delete" << endl;
-	cin.get();
-	getline(cin, name);
+	message = "Write name of movie you want delete\n";
+	name = answerStringViewer(message, 0, 2, 25);
 
 	int size = movies.size();
-	int sizeWatch = watchListMovies.size();
 
 	for (vector<Movie>::iterator i = movies.begin(); i != movies.end(); ++i) {//delete from vector of data base
-		if (i->getName() != name) continue;
-		if (i->getIsWL() != "Y") continue;
-		for (vector<Movie>::iterator j = watchListMovies.begin(); j != watchListMovies.end(); ++j) {//delete from vector watch list of user
-			if (j->getName() != name) continue;
-			watchListMovies.erase(j);
+		if (i->getName() == name) {
+			if (i->getIsWL() == "Y") {
+				for (vector<Movie>::iterator j = watchListMovies.begin(); j != watchListMovies.end(); ++j) {//delete from vector watch list of user
+					if (j->getName() == name) {
+						watchListMovies.erase(j);
+						break;
+					}
+				}
+			}
+			movies.erase(i);
 			break;
-			
 		}
-		movies.erase(i);
-		break;
 	}
+	
 	if (size == movies.size()) {
 		cout << "Movie with this name is not exist in data base" << endl;
 		return;
 	}
 
-	if (sizeWatch == watchListMovies.size()) {
-		cout << "Movie with this name is not exist in watch list" << endl;
-		return;
-	}
+	cout << "Movie deleted succesfully" << endl;
 }
 
 
 void Admin::findMovieByCategory() {
 	int answer = 0;
 	int counter = 1;
-	string category, name;
+	string category, name, message;
 	vector<Movie>& movies = MoviesDB::getMoviesDB();
 	vector<Movie>& watchListMovies = MoviesDB::getMoviesWatchListDB();
 
@@ -136,9 +131,9 @@ void Admin::findMovieByCategory() {
 		return;
 	}
 
-	cout << "Which movie you want delete" << endl;
-	cin >> answer;
-	
+	message = "Which movie you want delete\n";
+	answer = answerIntViewer(message, 1, counter);
+
 	counter=0;
 
 	for (vector<Movie>::iterator i = movies.begin(); i != movies.end(); ++i) {//delete from vector
@@ -146,11 +141,12 @@ void Admin::findMovieByCategory() {
 		counter++;
 		if (counter != answer) continue;
 		name = i->getName();
-		if (i->getIsWL() != "Y") break;
-		for (vector<Movie>::iterator j = watchListMovies.begin(); j != watchListMovies.end(); ++j) {//delete from vector
-			if (j->getName() == name) {
-				watchListMovies.erase(j);
-				break;
+		if (i->getIsWL() == "Y") {
+			for (vector<Movie>::iterator j = watchListMovies.begin(); j != watchListMovies.end(); ++j) {//delete from vector
+				if (j->getName() == name) {
+					watchListMovies.erase(j);
+					break;
+				}
 			}
 		}
 		movies.erase(i);
@@ -162,7 +158,7 @@ void Admin::findMovieByCategory() {
 
 void Admin::findSeriesByName(){
 	int answer = 0;
-	string name;
+	string name, message;
 	vector<Series>& series = SeriesDB::getSeriesDB();
 	vector<Series>& watchListSeries = SeriesDB::getSeriesWatchListDB();
 
@@ -171,24 +167,25 @@ void Admin::findSeriesByName(){
 		return;
 	}
 
-	cout << "Write name of series you want delete" << endl;
-	cin.get();
-	getline(cin, name);
+	message = "Write name of series you want delete\n";
+	name = answerStringViewer(message, 0, 2, 25);
 
 	int size = series.size();
-	int sizeSeries = watchListSeries.size();
 
 	for (vector<Series>::iterator i = series.begin(); i != series.end(); ++i) {//delete from vector of data base
-		if (i->getName() != name) continue;
-		if (i->getIsWL() != "Y") continue;
-		for (vector<Series>::iterator j = watchListSeries.begin(); j != watchListSeries.end(); ++j) {//delete from vector watch list of user
-			if (j->getName() != name) continue;
-			watchListSeries.erase(j);
-			break;
-
+		if (i->getName() == name) {
+		if (i->getIsWL() == "Y") {
+			for (vector<Series>::iterator j = watchListSeries.begin(); j != watchListSeries.end(); ++j) {//delete from vector watch list of user
+				if (j->getName() == name) {
+						watchListSeries.erase(j);
+						break;
+				}
+			}
+		
 		}
 		series.erase(i);
 		break;
+		}
 	}
 
 	if (size == series.size()) {
@@ -196,42 +193,13 @@ void Admin::findSeriesByName(){
 		return;
 	}
 
-	if (size == watchListSeries.size()) {
-		cout << "Series with this name is not exist in watch list" << endl;
-		return;
-	}
-
-}
-
-
-void Admin::getPersonalInfo()
-{
-	string lname, fname, byear, bmonth, bday, id;
-	string path = "admin.txt";
-	ifstream fin;
-	fin.open(path, ios::in);
-	if (!fin.is_open()) { cout << "file not found" << endl; }
-	else {
-			getline(fin, fname);
-			this->fname = fname;
-			getline(fin, lname);
-			this->lname = lname;
-			getline(fin, id);
-			this->id = stoi(id);
-			getline(fin, bday);
-			this->bday = stoi(bday);
-			getline(fin, bmonth);
-			this->bmonth = stoi(bmonth);
-			getline(fin, byear);
-			this->byear = stoi(byear);
-	}
-	fin.close();
+	cout << "Series deleted succesfully" << endl;
 }
 
 void Admin::findSeriesByCategory() {
 	int answer = 0;
 	int counter = 1;
-	string category, name;
+	string category, name, message;
 	vector<Series>& series = SeriesDB::getSeriesDB();
 	vector<Series>& watchListSeries = SeriesDB::getSeriesWatchListDB();
 
@@ -256,8 +224,9 @@ void Admin::findSeriesByCategory() {
 		return;
 	}
 
-	cout << "Which series you want delete" << endl;
-	cin >> answer;
+	message = "Which series you want delete\n";
+	answer = answerIntViewer(message, 1, counter);
+
 	counter = 0;
 
 	for (vector<Series>::iterator i = series.begin(); i != series.end(); ++i) {//delete from vector
@@ -265,11 +234,12 @@ void Admin::findSeriesByCategory() {
 		counter++;
 		if (counter != answer) continue;
 		name = i->getName();
-		if (i->getIsWL() != "Y") break;
-		for (vector<Series>::iterator j = watchListSeries.begin(); j != watchListSeries.end(); ++j) {//delete from vector
-			if (j->getName() == name) {
-				watchListSeries.erase(j);
-				break;
+		if (i->getIsWL() == "Y") {
+			for (vector<Series>::iterator j = watchListSeries.begin(); j != watchListSeries.end(); ++j) {//delete from vector
+				if (j->getName() == name) {
+					watchListSeries.erase(j);
+					break;
+				}
 			}
 		}
 		series.erase(i);
@@ -278,5 +248,29 @@ void Admin::findSeriesByCategory() {
 	cout << "Series deleted succesfully" << endl;
 }
 	
+void Admin::getPersonalInfo()
+{
+	string lname, fname, byear, bmonth, bday, id;
+	string path = "admin.txt";
+	ifstream fin;
+	fin.open(path, ios::in);
+	if (!fin.is_open()) { cout << "file not found" << endl; }
+	else {
+		getline(fin, fname);
+		this->fname = fname;
+		getline(fin, lname);
+		this->lname = lname;
+		getline(fin, id);
+		this->id = stoi(id);
+		getline(fin, bday);
+		this->bday = stoi(bday);
+		getline(fin, bmonth);
+		this->bmonth = stoi(bmonth);
+		getline(fin, byear);
+		this->byear = stoi(byear);
+	}
+	fin.close();
+}
+
 
 
